d2fecfcdb9bb0b438fa31ea4fe8878053d021637
roshinys <roshinchaya@gmail.com>
2023-08-01 15:11:41 +0530

    fixed subject spelling

--- models/Mail.js
+++ models/Mail.js
@@ -4,7 +4,7 @@ const mailSchema = new mongoose.Schema({
   sender: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
   receiver: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
   body: { type: String },
-  Subject: { type: String },
+  subject: { type: String },
   createdAt: { type: Date, default: Date.now },
 });
 
